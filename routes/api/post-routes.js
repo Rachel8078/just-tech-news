91c738ec8c9ca7f902d252c09f8147f85625493e
Rachel <rachelmmarriott@gmail.com>
2022-03-16 20:15:11 -0600

    update routes to include comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,10 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 router.get("/", (req, res) => {
-  console.log("======================");
   Post.findAll({
-    // Query configuration
+    order: [['created_at', 'DESC']],
     attributes: [
       "id", 
       "post_url", 
@@ -13,8 +12,16 @@ router.get("/", (req, res) => {
       "created_at",
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      // include the Comment model here:
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ["username"],
